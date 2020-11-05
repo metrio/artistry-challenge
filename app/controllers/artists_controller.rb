813977805315d401c32daf36f15c0dcce5eda35c
metrio <dlima.nyit@gmail.com>
2020-11-04 20:26:40 -0500

    yeah

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,9 +8,29 @@ class ArtistsController < ApplicationController
         @artist = Artist.find(params[:id])
     end
 
+    def new
+        @artist = Artist.new
+        render :new
+    end
+
+    def create
+        @artist = Artist.new(artist_params)
+
+        if @artist.save
+            redirect_to artist_path(@artist)
+        else
+            flash[:errors] = @artist.errors.full_messages
+            redirect_to new_artist_path
+        end
+    end
+
    
     
+private
 
+def artist_params
+    params.require(:artist).permit(:name,:age, :title)
+end
 
 
     
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,6 @@ class Artist < ApplicationRecord
     has_many :artist_instrument
     has_many :instrument, through: :artist_instrument
 
+    validates :name, presence: :true
+    validates :title, uniqueness: :true
 end
